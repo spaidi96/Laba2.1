a3a709a9190e2a7eccf9a188385e573c5d839abb
Roman Hrubryn <115115965+spaidi96@users.noreply.github.com>
2023-03-14 21:42:22 +0200

    Initial commit

---
+++ Laba2.1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Linq;
+namespace Laba2_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            do
+            {
+                Console.Write("Введiть число, яке завдання хочете обрати: ");
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        Console.WriteLine("Завдання 1:" + '\n' +
+                        "Дано текстовий файл. Створіть список, кожен елемент якого містить кількість символів у відповідному рядку тексту.");
+                        Console.WriteLine("---------------------------------------");
+                        string filepath = "D:\\Laba\\Text.txt";
+                        if (File.Exists(filepath))
+                        {
+                            string[] lines = File.ReadAllLines(filepath);
+                            Console.WriteLine("Текст:");
+                            foreach (string line in lines)
+                            {
+                                Console.WriteLine(line);
+                            }
+
+                            Console.WriteLine("---------------------------------------");
+                        }
+                        List<int> lengths = new List<int>();
+                        using (StreamReader reader = new StreamReader(filepath))
+                        {
+                            string line;
+                            while ((line = reader.ReadLine()) != null)
+                            {
+                                lengths.Add(line.Length);
+                            }
+                        }
+                        Console.WriteLine("Кількість символів у кожному рядку:");
+                        foreach (int length in lengths)
+                        {
+                            Console.WriteLine(length);
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Завдання 2" + '\n' +
+                        "Порахувати кількість записів у словнику, значення яких є списком. Записати словник у JSON");
+                        Console.WriteLine("---------------------------------------");
+                        int count = 0;
+                        var dict = new Dictionary<string, object>();
+                        dict.Add("key1", new List<int> { 1, 2, 3 });
+                        dict.Add("key2", "value");
+                        dict.Add("key3", new List<string> { "Mama", "Papa" });
+                        dict.Add("key4", 4);
+                        dict.Add("key5", new List<double> {123f});
+                        foreach (var value in dict.Values)
+                        {
+                            if (value is IList)
+                            {
+                                count++;
+                            }
+                        }
+                        Console.WriteLine("Кількість записів у словнику, значення яких є списком: " + count);
+                        Console.WriteLine("---------------------------------------");
+                        string json = JsonConvert.SerializeObject(dict);
+                        Console.WriteLine(json);
+                        break;
+                    case 3:
+                        Console.WriteLine("Завдання 3:" + '\n' +
+                        "Дана послідовність непустих рядків. Використовуючи метод Aggregate, отримати рядок, що" + '\n' +
+                        "складається з початкових символів всіх рядків вихідної послідовності.");
+                        Console.WriteLine("---------------------------------------");
+                        List<string> list = new List<string> { "i", "linq", "orange", "value", "earth", "cat", "#" };
+                        foreach (var i in list)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        Console.WriteLine("---------------------------------------");
+                        string newlist = list.Aggregate("", (a, b) => a + b[0]);
+                        Console.WriteLine(newlist);
+                        break;
+                }
+                Console.WriteLine();
+            Console.WriteLine("Для закінчення програми напишіть \"вийти\", щоб продовжити, натисніть будь-яку кнопку");
+        } while (Console.ReadLine().ToLower() != "вийти");
+        }
+    }
+}
---
+++ Laba2.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Laba2._1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Laba2._1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("CB7F2613-A474-43B3-BE95-71DF449EF6D0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
